714514edea914fd3288958605af591b212dada2c
Flavio Percoco <flaper87@gmail.com>
2018-03-16 20:03:04 +0100

    Allow for bind-mounting local tht

--- run.sh
+++ run.sh
@@ -1,15 +1,23 @@
-#TEMPLATES=/usr/share/openstack-tripleo-heat-templates/
+#!/bin/bash
 set -x
-TEMPLATES=/root/tripleo-heat-templates/
+CONTAINER_THT=/root/tripleo-heat-templates/
 
 mkdir /tmp/root
 
-docker run --volume /tmp/root:/root/ tripleoclient:custom /usr/bin/openstack overcloud container image prepare \
+CONTAINER_OPTS=""
+
+if [ -z "${LOCAL_THT+x}" ];
+then
+  CONTAINER_THT=/usr/share/openstack-tripleo-heat-templates/
+  CONTAINER_OPTS="$CONTAINER_OPTS -v $LOCAL_THT:$CONTAINER_THT"
+fi
+
+docker run $CONTAINER_OPTS --volume /tmp/root:/root/ tripleoclient:custom /usr/bin/openstack overcloud container image prepare \
 --tag tripleo-ci-testing \
 --namespace trunk.registry.rdoproject.org/master \
 --output-env-file=/root/containers.yaml \
 --template-file /usr/share/tripleo-common/container-images/overcloud_containers.yaml.j2 \
--r $TEMPLATES/roles_data_undercloud.yaml
+-r $CONTAINER_THT/roles_data_undercloud.yaml
 
 sudo bash <<EOF_BASH
 cat >> /tmp/root/containers.yaml <<EOF_CAT
@@ -21,17 +29,17 @@ EOF_CAT
 EOF_BASH
 
 set -e
-time docker run --net=host --privileged --volume /tmp/root/:/root/tmp/ tripleoclient:custom /usr/bin/openstack undercloud deploy \
+time docker run --net=host --privileged $CONTAINER_OPTS --volume /tmp/root/:/root/tmp/ tripleoclient:custom /usr/bin/openstack undercloud deploy \
 --local-ip=172.30.0.1 \
---templates=$TEMPLATES \
+--templates=$CONTAINER_THT \
 --output-only \
 --output-dir=/root/tmp/ \
 -e /root/tmp/containers.yaml \
--e $TEMPLATES/environments/docker.yaml \
--e $TEMPLATES/environments/net-noop.yaml \
--e $TEMPLATES/environments/services-docker/tripleo-ui.yaml \
--e $TEMPLATES/environments/config-download-environment.yaml \
--r $TEMPLATES/roles_data_undercloud.yaml
+-e $CONTAINER_THT/environments/docker.yaml \
+-e $CONTAINER_THT/environments/net-noop.yaml \
+-e $CONTAINER_THT/environments/services-docker/tripleo-ui.yaml \
+-e $CONTAINER_THT/environments/config-download-environment.yaml \
+-r $CONTAINER_THT/roles_data_undercloud.yaml
 set +e
 
 TMPDIR=$(ls -ltr /tmp/root | grep "tripleo-.*-config" | awk '{print $9}' | tail -n 1)
